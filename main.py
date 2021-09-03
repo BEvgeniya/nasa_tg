2962a8ca3813b12ab7316084a952cf7e8a0a20d0
BEvgeniya <65121485+BEvgeniya@users.noreply.github.com>
2021-09-03 23:22:18 +0500

    Исправлены ошибки в коде

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 import datetime
 
 
-def loadpicture(url, path):
+def load_picture(url, path):
     headers = {
         'User-Agent': 'curl',
         'Accept-Language': 'ru-RU'
@@ -20,25 +20,27 @@ def loadpicture(url, path):
         return file.write(response.content)
 
 
-def fetch_spacex_last_launch():
+def fetch_spacex_last_launch(folder):
+    os.makedirs(folder)
     spacex_url = 'https://api.spacexdata.com/v4/launches'
     response = requests.get(spacex_url)
     response.raise_for_status()
     launches = response.json()
     for launch in launches:
         spacex_image_links = launch['links']['flickr']['original']
-        if spacex_image_links != []:
+        if spacex_image_links:
             for index, image_url in enumerate(spacex_image_links):
-                filename = 'images/space' + str(index) + '.jpg'
-                loadpicture(image_url, filename)
+                filename = f'{folder}/space{str(index)}.jpg'
+                load_picture(image_url, filename)
 
 
-def get_nasa_day_pictures(nasa_apitoken):
+def get_nasa_day_pictures(folder, nasa_api_token):
     params = {
+        'api_key': nasa_api_token,
         'count': 30
     }
-
-    url = f'https://api.nasa.gov/planetary/apod?api_key={nasa_apitoken}'
+    os.makedirs(folder)
+    url = 'https://api.nasa.gov/planetary/apod'
     response = requests.get(url, params=params)
     response.raise_for_status()
     image_urls = []
@@ -51,63 +53,54 @@ def get_nasa_day_pictures(nasa_apitoken):
 
     for index, image_url in enumerate(image_urls):
         extension = get_file_extension(image_url)
-        if extension != '':
-            filename = 'nasa_dayly/nasa_day' + str(index) + extension
-            loadpicture(image_url, filename)
+        if extension:
+            filename = f'{folder}/nasa_day{str(index)}{extension}'
+            load_picture(image_url, filename)
 
 
 def get_file_extension(url):
     parsed_url = urlparse(url)
     parsed_url_path = parsed_url.path
-    splitted_path = os.path.split(parsed_url_path)[1]
-    extension = os.path.splitext(splitted_path)[1]
+    extension = os.path.splitext(parsed_url_path)[1]
     return extension
 
 
-def get_correct_data(date_str):
-    image_date = datetime.datetime.fromisoformat(date_str)
-    year = image_date.year
-    month = image_date.month
-    day = image_date.day
-    if month < 10:
-        month = '0' + str(month)
-    if day < 10:
-        day = '0' + str(day)
-    return [str(year), month, day]
-
-
-def get_nasa_epic_pictures(nasa_apitoken):
-    url = f'https://api.nasa.gov/EPIC/api/natural?api_key={nasa_apitoken}'
-    response = requests.get(url)
+def get_nasa_epic_pictures(folder, nasa_api_token):
+    os.makedirs(folder)
+    params = {
+        'api_key': nasa_api_token
+    }
+    url = f'https://api.nasa.gov/EPIC/api/natural'
+    response = requests.get(url, params=params)
     response.raise_for_status()
 
     for index, image_info in enumerate(response.json()):
         image_name = image_info['image']
-        image_date = get_correct_data(image_info['date'])
+        image_date = datetime.datetime.fromisoformat(image_info['date'])
+        base_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date.year}/{image_date.strftime("%m")}/{image_date.strftime("%d")}/png/{image_name}.png?api_key={nasa_api_token}'
 
-        base_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date[0]}/{image_date[1]}/{image_date[2]}/png/{image_name}.png?api_key={nasa_apitoken}'
+        path = f'{folder}/planet{str(index)}.png'
+        load_picture(base_url, path)
 
-        path = 'earth_epic_photos/planet' + str(index) + '.png'
-        loadpicture(base_url, path)
+
+def post_photos(all_folders, bot):
+    chat_id = os.environ['TG_CHAT_ID']
+    while True:
+        for folder in all_folders:
+            for file in listdir(folder):
+                time.sleep(60 * 60 * 24)
+                with open(f'{folder}/{file}', 'rb') as document:
+                    bot.send_document(chat_id=chat_id, document=document)
 
 
 def main():
-    nasa_apitoken = os.environ['NASA_APITOKEN']
+    nasa_api_token = os.environ['NASA_API_TOKEN']
     tg_bot_token = os.environ['TG_BOT_TOKEN']
-    chat_id = os.environ['TG_CHAT_ID']
     bot = telegram.Bot(token=tg_bot_token)
-    fetch_spacex_last_launch()
-    get_nasa_day_pictures(nasa_apitoken)
-    get_nasa_epic_pictures(nasa_apitoken)
-
-    while True:
-        all_folders = ['earth_epic_photos', 'images', 'nasa_dayly']
-        for folder in all_folders:
-            for file in listdir(folder):
-                time.sleep(10)
-                bot.send_document(chat_id=chat_id,
-                                  document=open(folder + '/' + file, 'rb'))
+    all_folders = ['earth_epic_photos', 'images', 'nasa_dayly']
+    post_photos(all_folders, bot)
 
 
 if __name__ == '__main__':
     main()
+
